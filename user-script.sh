205fd2893070d648f503c6a1fe6b9f181e88835e
Salman Siddiqui <salmansiddiqui172002@gmail.com>
2025-10-24 14:26:20 +0300

    project-3 (original)

---
+++ user-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ex 
+
+wget https://aws-tc-largeobjects.s3-us-west-2.amazonaws.com/DEV-AWS-MO-GCNv2/FlaskApp.zip 
+unzip FlaskApp.zip 
+cd FlaskApp/ 
+yum -y install python3-pip 
+pip install -r requirements.txt 
+yum -y install stress 
+export PHOTOS_BUCKET=aws-flask-app-147779121814 
+
+
+export AWS_DEFAULT_REGION=us-east-1
+export DYNAMO_MODE=on 
+export FLASK_APP=application.py 
+
+/usr/local/bin/flask run --host=0.0.0.0 --port=80
